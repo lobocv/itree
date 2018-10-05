e80fbadabf1f7222fd5214c576ea6dbb20067839
Calvin Lobo <calvinlobo@gmail.com>
2018-10-05 12:24:13 -0400

    Made DirContext a linked list to provide tree-navigation

--- itree.go
+++ itree.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
 	"github.com/nsf/termbox-go"
@@ -36,6 +37,10 @@ func GetScreen(dir *DirContext) Screen {
 	return Screen{dir, w, h, make([]rune, 0, 100), Directory}
 }
 
+func (s *Screen) SetDirectory(dir *DirContext) {
+	s.dir = dir
+}
+
 func (s *Screen) Print(x, y int, fg, bg termbox.Attribute, msg string) {
 	for _, c := range msg {
 		termbox.SetCell(x, y, c, fg, bg)
@@ -43,39 +48,100 @@ func (s *Screen) Print(x, y int, fg, bg termbox.Attribute, msg string) {
 	}
 }
 
-func (s *Screen) PrintDirContents() error {
-	_, height := termbox.Size()
-	var x, y int
-	s.Print(x, y, termbox.ColorRed, termbox.ColorDefault, s.dir.AbsPath)
-	for ii, f := range s.dir.Files {
-		var color termbox.Attribute
-		var itemname string
+func (s *Screen) PrintDirContents(upperLevels int) error {
+	var levelOffsetX, levelOffsetY int // Draw position offset
+	var stretch int                    // Length of line connecting subdirectories
+	var maxLineWidth int               // Length of longest item in the directory
 
-		if s.dir.FileIdx == ii {
-			color = termbox.ColorCyan
-		} else {
+	// Print the current path
+	s.Print(levelOffsetX, levelOffsetY, termbox.ColorRed, termbox.ColorDefault, s.dir.AbsPath)
+	levelOffsetY += 2
+
+	// Create a slice of the directory chain containing upperLevels number of parents
+	dirlist := make([]*DirContext, 0, 1+upperLevels)
+	next := s.dir.Parent
+	for ii := 0; next != nil; ii++ {
+		dirlist = append([]*DirContext{next}, dirlist...)
+		next = next.Parent
+		if ii >= upperLevels {
+			break
+		}
+	}
+	dirlist = append(dirlist, s.dir)
+
+	// Recurse through the directory list, drawing a tree structure
+	for level, dir := range dirlist {
+		maxLineWidth = 0
+
+		for ii, f := range dir.Files {
+
+			// Keep track of the longest length item in the directory
+			filename_len := len(f.Name())
+			if filename_len > maxLineWidth {
+				maxLineWidth = filename_len
+			}
+
+			// Determine the color of the currently printing directory item
+			var color termbox.Attribute
+			if dir.FileIdx == ii {
+				color = termbox.ColorCyan
+			} else {
+				if f.IsDir() {
+					color = termbox.ColorYellow
+				} else {
+					color = termbox.ColorWhite
+				}
+
+			}
+
+			line := bytes.Buffer{}
+			if ii == 0 {
+				line.WriteString(strings.Repeat("─", stretch))
+			}
+
+			switch ii {
+			case 0:
+				if level > 0 {
+					if len(dir.Files) < 2 {
+						line.WriteString("─")
+					} else {
+						line.WriteString("┬─")
+					}
+				} else {
+					line.WriteString("├─")
+				}
+			case len(dir.Files) - 1:
+				line.WriteString("└─")
+			default:
+				line.WriteString("├─")
+			}
+
+			// Create the item label, add / if it is a directory
+			line.WriteString(f.Name())
 			if f.IsDir() {
-				color = termbox.ColorYellow
-			} else {
-				color = termbox.ColorWhite
+				line.WriteString("/")
 			}
 
-		}
-		itemname = f.Name()
-		if f.IsDir() {
-			itemname += "/"
-		}
-		row := (y + ii + 1) % height
-		col := int((y + ii + 1) / height)
-		if row < height {
-			var prefix string
-			if ii == len(s.dir.Files)-1 {
-				prefix += "└─"
-			} else {
-				prefix += "├─"
+			// Calculate the draw position
+			x := levelOffsetY + ii
+			y := levelOffsetX
+			if ii == 0 {
+				// The first item is connected to the parent directory with a line
+				// shift the position left to account for this line
+				y -= stretch
 			}
-			s.Print(col*20, row, color, termbox.ColorDefault, prefix+itemname)
+			s.Print(y, x, color, termbox.ColorDefault, line.String())
 		}
+
+		// Determine the length of line we need to draw to connect to the next directory
+		if len(dir.Files) > 0 {
+			stretch = maxLineWidth - len(dir.Files[dir.FileIdx].Name())
+		}
+
+		// Shift the draw position in preparation for the next directory
+		levelOffsetY += dir.FileIdx
+		levelOffsetX += maxLineWidth + 2
+
 	}
 
 	return nil
@@ -88,14 +154,14 @@ func (s *Screen) SetState(state ScreenState) {
 	s.state = state
 }
 
-func (s *Screen) Draw() {
+func (s *Screen) Draw(upperLevels int) {
 	s.ClearScreen()
 	switch s.state {
 	case Help:
 		s.Print(0, 0, termbox.ColorWhite, termbox.ColorDefault, "itree - An interactive tree application for file system navigation.")
 		s.Print(0, 2, termbox.ColorWhite, termbox.ColorDefault, "h - Toggle hidden files and folders.")
 	case Directory:
-		s.PrintDirContents()
+		s.PrintDirContents(upperLevels)
 	case Search:
 		s.Print(0, 0, termbox.ColorWhite, termbox.ColorDefault, "Enter a search string:")
 		s.Print(0, 2, termbox.ColorWhite, termbox.ColorDefault, string(s.SearchString))
@@ -117,6 +183,7 @@ type DirContext struct {
 	Files      []os.FileInfo
 	FileIdx    int
 	ShowHidden bool
+	Parent     *DirContext
 }
 
 // Methods for filtering files by directory, then file
@@ -160,30 +227,42 @@ func (d *DirContext) SetDirectory(path string) error {
 	return nil
 }
 
-func (d *DirContext) Ascend() error {
+func (d *DirContext) Ascend() (*DirContext, error) {
 	newpath := path.Dir(d.AbsPath)
-	err := d.SetDirectory(newpath)
-	for idx, f := range d.Files {
-		if f.Name() == newpath {
-			d.FileIdx = idx
-			break
+	if d.Parent != nil {
+		return d.Parent, nil
+	} else {
+		parent := new(DirContext)
+
+		err := parent.SetDirectory(newpath)
+		if err != nil {
+			return nil, err
 		}
+
+		for idx, f := range parent.Files {
+			if f.Name() == newpath {
+				parent.FileIdx = idx
+				break
+			}
+		}
+		return parent, err
 	}
-	return err
+
 }
 
-func (d *DirContext) Descend() error {
+func (d *DirContext) Descend() (*DirContext, error) {
 	if len(d.Files) == 0 {
-		return nil
+		return nil, nil
 	}
+	child := new(DirContext)
 	f := d.Files[d.FileIdx]
 	if f.IsDir() {
 		newpath := path.Join(d.AbsPath, f.Name())
-		d.SetDirectory(newpath)
-
-		return nil
+		child.SetDirectory(newpath)
+		child.Parent = d
+		return child, nil
 	} else {
-		return errors.New("Cannot enter non-directory.")
+		return nil, errors.New("Cannot enter non-directory.")
 	}
 }
 
@@ -218,7 +297,8 @@ Application
 
 func main() {
 	var inputmode = false
-	err := termbox.Init()
+	var err error
+	err = termbox.Init()
 
 	if err != nil {
 		panic(err)
@@ -234,14 +314,14 @@ func main() {
 		panic("Cannot get absolute directory.")
 	}
 
-	dir := DirContext{}
+	dir := new(DirContext)
 	dir.SetDirectory(cwd)
-	screen := GetScreen(&dir)
+	screen := GetScreen(dir)
 
 MainLoop:
 	for {
-
-		screen.Draw()
+		screen.SetDirectory(dir)
+		screen.Draw(2)
 
 		ev := termbox.PollEvent()
 		if inputmode {
@@ -268,15 +348,20 @@ MainLoop:
 			switch ev.Key {
 			case termbox.KeyEsc, termbox.KeyCtrlC:
 				break MainLoop
-
 			case termbox.KeyArrowUp:
 				dir.MoveSelector(-1)
 			case termbox.KeyArrowDown:
 				dir.MoveSelector(1)
 			case termbox.KeyArrowLeft:
-				dir.Ascend()
+				nextdir, err := dir.Ascend()
+				if nextdir != nil && err == nil {
+					dir = nextdir
+				}
 			case termbox.KeyArrowRight:
-				dir.Descend()
+				nextdir, err := dir.Descend()
+				if nextdir != nil && err == nil {
+					dir = nextdir
+				}
 			case termbox.KeyCtrlH:
 				if screen.GetState() != Help {
 					screen.SetState(Help)
@@ -298,7 +383,6 @@ MainLoop:
 				break MainLoop
 			case 'h':
 				dir.SetShowHidden(!dir.ShowHidden)
-
 			}
 
 		case termbox.EventResize:
