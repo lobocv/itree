f81ddf7d0d9467b123f1edbd98019b0d0ee5fa50
Calvin Lobo <calvinl@patriot1tech.com>
2018-10-11 13:45:32 -0400

    added test for checking filter

--- dir_test.go
+++ dir_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"os"
 	"testing"
 	"github.com/lobocv/itree/ctx"
@@ -8,15 +10,25 @@ import (
 
 var testDirRoot = "/tmp/itree"
 
+var dirpaths = []string {testDirRoot + "/a/a1/a2",
+					  	 testDirRoot + "/b/b1/b2"}
+
+var filepaths = []string {"/a/f1", "/a/f2", "/a/f3", "/a/a1/f1", "/a/a1/a2/f2", "/a/a1/a2/f3"}
+
+
 func setUp() error {
-
-	var paths = []string { testDirRoot + "/a/a1/a2",
-	 					   testDirRoot + "/b/b1/b2"}
-
-	for _, p := range paths {
+		for _, p := range dirpaths {
 		err := os.MkdirAll(p, 0777)
 		if err != nil {
 			return err
+		}
+	}
+
+	for _, fp := range filepaths {
+		var contents []byte
+		err := ioutil.WriteFile(testDirRoot + fp, contents, 0777)
+		if err != nil {
+			panic(err)
 		}
 	}
 
@@ -24,11 +36,41 @@ func setUp() error {
 }
 
 func tearDown() {
+	for _, fp := range filepaths {
+		err:= os.Remove(testDirRoot + fp)
+		if err != nil {
+			panic(fmt.Sprintf("Cannot remove file %v", testDirRoot + fp))
+		}
+	}
+}
+func TestFilterContents(t *testing.T) {
+	err := setUp()
+	if err != nil {
+		t.Error(err)
+	}
+	defer tearDown()
+
+	curDir, err := getDirChain()
+	if err != nil {
+		t.Error(err)
+	}
+	a := curDir.Parent.Parent
+	a.FilterContents("f")
+	if len(a.FilteredFiles) != 3 {
+		t.Error("Expected 3 files")
+	}
 
 }
 
+
+func getDirChain() (*ctx.Directory, error) {
+	cwd := testDirRoot + "/a/a1/a2"
+
+	curDir, err := ctx.CreateDirectoryChain(cwd)
+	return curDir, err
+}
+
 func TestCreateDirectoryChain(t *testing.T) {
-	var curDir *ctx.Directory
 
 	err := setUp()
 	if err != nil {
@@ -36,9 +78,8 @@ func TestCreateDirectoryChain(t *testing.T) {
 	}
 	defer tearDown()
 
-	cwd := testDirRoot + "/a/a1/a2"
 
-	curDir, err = ctx.CreateDirectoryChain(cwd)
+	curDir, err := getDirChain()
 	if err != nil {
 		t.Error(err)
 	}
